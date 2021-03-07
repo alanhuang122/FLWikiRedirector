18dbb0fdb3e6d690e4ccbb88b97c5cd871df232f
Alan Huang <alan@alanhua.ng>
2021-03-07 06:19:02 +0000

    Update extension to use Manifest v3

--- background.js
+++ background.js
@@ -1,53 +1,25 @@
-// Simple extension to redirect all requests to FL Wikia to FL Wiki
-(function(){
-  'use strict';
-  let isPluginDisabled = false; // Variable storing whether or not the plugin is disabled.
-  let storage = window.storage || chrome.storage; // Make sure we have a storage API.
+let storage = window.storage || chrome.storage; // Make sure we have a storage API.
 
-  const RSWIKIA_REGEX = /^fallenlondon\.(wikia|fandom)\.com$/i;
-
-  // Listen to before anytime the browser attempts to navigate to the old Wikia/Fandom sites.
-  chrome.webNavigation.onBeforeNavigate.addListener(
-    function(info) {
-      if(isPluginDisabled) { // Ignore all navigation requests when the extension is disabled.
-        console.log("FLWikia intercepted, ignoring because plugin is disabled.");
-        return;
-      }
-
-      // Create a native URL object to more easily determine the path of the url and the domain.
-      const url = new URL(info.url);
-
-      const isWikia = RSWIKIA_REGEX.test(url.host); // Check to ensure the redirect is occurring on either the fandom/wikia domain.
-      // If domain isn't subdomain of wikia.com, ignore, also if it's not in the redirect filter
-      if (!isWikia) return;
-
-      // Generate new url
-      const redirectUrl = `https://fallenlondon.wiki${url.pathname}`; // Create the redirect URL
-      console.log(`FLWikia intercepted:  ${info.url}\nRedirecting to ${redirectUrl}`); 
-      // Redirect the old wikia request to new wiki
-      chrome.tabs.update(info.tabId,{url:redirectUrl});
-    });
-
-  function updateIcon(){
-    // Change the icon to match the state of the plugin.
-    chrome.browserAction.setIcon({ path: isPluginDisabled?"icon32_disabled.png":"icon32.png"  });
-  }
-
+function updateIcon(){
   storage.local.get(['isDisabled'],(result)=>{
-      // Get the initial condition of whether or not the extension is disabled
+  // Change the icon to match the state of the plugin.
       isPluginDisabled= result ? result.isDisabled : false;
-      updateIcon(); // Update icon to match new state
+      chrome.action.setIcon({ path: isPluginDisabled?"icon32_disabled.png":"icon32.png"  });
   });
+}
 
-  // Anytime the state of the plugin changes, update the internal state of the background script.
-  storage.onChanged.addListener(
-      function(changes, areaName) {
-        // If isDisabled changed, update isPluginDisabled
-        if(changes["isDisabled"]!==undefined && changes["isDisabled"].newValue!=changes["isDisabled"].oldValue) {
-          console.log(`FL Wiki Redirector is now ${changes["isDisabled"].newValue?'disabled':'enabled'}`);
-          isPluginDisabled=changes["isDisabled"].newValue;
-          updateIcon();
-        }
+storage.onChanged.addListener(
+  function(changes, areaName) {
+    // If isDisabled changed, update isPluginDisabled
+    if(changes["isDisabled"]!==undefined && changes["isDisabled"].newValue!=changes["isDisabled"].oldValue) {
+      console.log(`FL Wiki Redirector is now ${changes["isDisabled"].newValue?'disabled':'enabled'}`);
+      if(changes["isDisabled"].newValue) {
+        declarativeNetRequest.updateEnabledRulesets({"disableRulesetIds": ["fandom_rule"]});
       }
-    );
-})();
+      else {
+        declarativeNetRequest.updateEnabledRulesets({"enableRulesetIds": ["fandom_rule"]});
+      }
+      updateIcon();
+    }
+  }
+);
